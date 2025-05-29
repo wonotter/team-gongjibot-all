86002865e6b30ff1fc6acbd1a7fc741c665e4118
wonotter <zjaxhfl99@kumoh.ac.kr>
2025-05-30 02:24:02 +0900

    FEAT: React와 SpringBoot 연동
    📌 외부에서 접속 가능하도록 포트 포워딩 및 DDNS 경로 추가
    
    📌 LandingPage.css, LandingPage.js와 같이 불필요한 파일 삭제

--- src/main/java/com/gongjibot/ragchat/config/CorsConfig.java
+++ src/main/java/com/gongjibot/ragchat/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해 적용
-                .allowedOrigins("http://localhost:3000")  // React 앱 주소
+                .allowedOrigins("http://wonokim.iptime.org:3000")  // React 앱 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization", "Authorization-refresh") // 헤더에 있는 JWT 정보를 받을 수 있도록 노출시킴
